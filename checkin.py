1ed362413fd96a049fc8f721765ff7228d856f33
fa1247 <xyfan@live.it>
2020-09-11 21:59:17 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,1 +1,6 @@
-print('ok')
+import requests
+
+bot_token = "98964309:AAHMRuqeOe7lOySG2k3n0MwktQzXMLkn6ik"
+chat_id = "86415403"
+text = "action test"
+requests.get("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"%(bot_token,chat_id,text))
