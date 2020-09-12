b35bfa53ee16469746b21570cf05b80dffa9fae0
fa1247 <xyfan@live.it>
2020-09-12 08:50:03 +0800

    change time to UTC

--- checkin.py
+++ checkin.py
@@ -106,18 +106,18 @@ three_data = {
     'askforleave': '0'
 }
 
-if time.localtime()[3] == 8:
+if time.localtime()[3] == 0:
     response = requests.post(url=three_url,headers=headers,cookies=cookies,data=three_data)
     text = '每日打卡:'+json.loads(response.text)['m']
     requests.get("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"%(bot_token,chat_id,text))
     response = requests.post(url=one_url,headers=headers,cookies=cookies,data=one_data)
     text = '早打卡:'+json.loads(response.text)['m']
     requests.get("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"%(bot_token,chat_id,text))
-elif time.localtime()[3] == 12:
+elif time.localtime()[3] == 4:
     response = requests.post(url=three_url,headers=headers,cookies=cookies,data=three_data)
     text = '午打卡:'+json.loads(response.text)['m']
     requests.get("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"%(bot_token,chat_id,text))
-elif time.localtime()[3] == 20:
+elif time.localtime()[3] == 12:
     response = requests.post(url=three_url,headers=headers,cookies=cookies,data=three_data)
     text = '晚打卡:'+json.loads(response.text)['m']
     requests.get("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"%(bot_token,chat_id,text))
