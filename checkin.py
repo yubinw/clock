1a8ae641d12654a53530a3e2f3f2cdee7a9ea702
fa1247 <13291029@bjtu.edu.cn>
2020-09-12 08:56:34 +0800

    updata cookies

--- checkin.py
+++ checkin.py
@@ -10,7 +10,13 @@ three_url = 'https://newweixin.bjtu.edu.cn/xisuncov/wap/open-report/save'
 bot_token = os.getenv('BOT_TOKEN')
 chat_id = os.getenv('CHAT_ID')
 cookies = os.getenv('COOKIES')
+# uukey = os.getenv('UUKEY')
+# esi-sess = os.getenv('ESISESS')
 cookies = json.loads(cookies)
+# cookies = {
+#     'UUkey': '2f71f9f2e1ec16f53f50690d67a0c2cd',
+#     'eai-sess': '7u2ga1uqnmha79cbn6dro9pc97',
+# }
 
 
 headers = {
