520316c434312f18a34f608af5797e8c1fb10188
mac <mac@MacBook-Air.local>
2020-09-13 09:32:14 +0800

    narrow cron

--- checkin.py
+++ checkin.py
@@ -125,7 +125,8 @@ elif time.localtime()[3] == 12:
     response = requests.post(url=three_url,headers=headers,cookies=cookies,data=three_data)
     text = '晚打卡:'+json.loads(response.text)['m']
     requests.get("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"%(bot_token,chat_id,text))
-# else:
+else:
+    print('Bad timing. Do nothing.')
 #     response = requests.post(url=three_url,headers=headers,cookies=cookies,data=three_data)
 #     text = '重复打卡:'+json.loads(response.text)['m']
 #     requests.get("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"%(bot_token,chat_id,text))
